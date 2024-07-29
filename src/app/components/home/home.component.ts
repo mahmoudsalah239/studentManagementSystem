5c384a760e7a9838a76c23bd07fb8b7510f3f152
mahmoudsalah239 <mamosalah239@gmail.com>
2024-07-29 17:04:44 +0300

    landing page

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { NgxTypedJsModule } from 'ngx-typed-js';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [NgxTypedJsModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA] 
 })
 export class HomeComponent {
 
+  typedOptions = {
+    strings: ['Welcome to Angular 17!', 'Enjoy using ngx-typed-js.'],
+    typeSpeed: 50,
+    backSpeed: 25,
+    backDelay: 1000,
+    startDelay: 500,
+    loop: true
+  };
 }
